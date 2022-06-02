8938c1adeced8dc06f8d47da7c9c5630fd14ae8a
Trevor Bruner <trevor.a.bruner@gmail.com>
2022-06-02 18:22:08 -0400

    Remove length property from Ship obj and add boolean return on successful hit

--- Ship.js
+++ Ship.js
@@ -1,20 +1,23 @@
 const ship = (length) => {
   let hits = new Array(length).fill(false);
+  const shipLength = length;
   
   const hit = (square) => {
+    if(hits[square]) return false;
     hits[square] = true;
+    return true;
   }
 
   const isSunk = () => {
     let i = 0;
-    while(i < length) {
+    while(i < shipLength) {
       if(!hits[i]) return false;
       i++;
     }
 
     return true;
   }
-  return { length, hit, isSunk }
+  return { hit, isSunk }
 }
 
 export { ship }
--- ship.spec.js
+++ ship.spec.js
@@ -3,18 +3,21 @@ import { ship } from './Ship.js';
 const five = ship(5);
 
 describe('Create ship object', () => {
-  test('Ship object has correct length', () => {
-    expect(five.length).toBe(5);
-  });
-
   test('Ship isSunk is false by default', () => {
     expect(five.isSunk()).toBe(false);
   });
 
+  test('Ship hit returns true when unhit section is hit', () => {
+    expect(five.hit(0)).toBe(true);
+  });
+
   test('Ship isSunk is false after one hit', () => {
-    five.hit(0);
     expect(five.isSunk()).toBe(false);
   });
+
+  test('Shipt hit returns false if attempt to hit section already hit', () => {
+    expect(five.hit(0)).toBe(false);
+  })
 
   test('Ship isSunk is true after all five hit', () => {
     five.hit(1);
