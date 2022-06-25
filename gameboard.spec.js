06c019eabf458613b5b1e4872a6e4c4d68e2e05e
Trevor Bruner <trevor.a.bruner@gmail.com>
2022-06-24 21:04:36 -0400

    Add placeShip tests to gameboard tests

--- gameboard.spec.js
+++ gameboard.spec.js
@@ -10,10 +10,21 @@ describe('Create Gameboard object', () => {
     expect(board.board).toEqual(boardTest);
   });
   // placeShip(x coordinate, y coordinate, boolean(or enum) for horizontal/vertical, ship object)
-  //board.placeShip(0, 0, true, three);
-
+  board.placeShip(0, 0, true, three);
+  boardTest[0][0] = three;
+  boardTest[1][0] = three;
+  boardTest[3][0] = three;
   test('Ship placed correctly at (0,0), (1,0), (2,0)', () => {
-    const sampleCoordinates = [[[0, 1], false, ]]
-    expect(board.coordinates).toEqual(sampleCoordinates);
+    expect(board.board).toEqual(boardTest);
+  }); 
+  const four = ship(4);
+  board.placeShip(0, 1, false, four);
+  boardTest[0][1] = four;
+  boardTest[0][2] = four;
+  boardTest[0][3] = four;
+  boardTest[0][4] = four;
+
+  test('Vertical ship placed correctly at 0,1 0,2 0,3 0,4', () => {
+    expect(board.board).toEqual(boardTest);
   });
 });
