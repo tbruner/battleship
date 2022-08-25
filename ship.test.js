0fc34f177fab0c258541b01d34cf37b9022e9f48
tbruner <trevor.a.bruner@gmail.com>
2022-08-25 18:13:54 -0400

    Update ship module tests

---
+++ ship.test.js
@@ -0,0 +1,33 @@
+import { ship } from './Ship.js';
+
+const five = ship(5);
+
+describe('Create ship object', () => {
+  test('Ship isSunk is false by default', () => {
+    expect(five.isSunk()).toBe(false);
+  });
+
+  test('Ship has correct length', () => {
+    expect(five.getLength()).toBe(5);
+  });
+
+  test('Ship hit returns true when unhit section is hit', () => {
+    expect(five.hit(0)).toBe(true);
+  });
+
+  test('Ship isSunk is false after one hit', () => {
+    expect(five.isSunk()).toBe(false);
+  });
+
+  test('Shipt hit returns false if attempt to hit section already hit', () => {
+    expect(five.hit(0)).toBe(false);
+  })
+
+  test('Ship isSunk is true after all five hit', () => {
+    five.hit(1);
+    five.hit(2);
+    five.hit(3);
+    five.hit(4);
+    expect(five.isSunk()).toBe(true);
+  });
+});
