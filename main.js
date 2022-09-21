cdc19684fb37d09d2cc1e7d6a7ddec2b8d2998c2
tbruner <trevor.a.bruner@gmail.com>
2022-09-21 11:26:03 -0400

    Set up game loop

--- main.js
+++ main.js
@@ -47,6 +47,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "Gameboard": () => (/* binding */ Gameboard)
 /* harmony export */ });
+/* harmony import */ var _Ship_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(3);
+
+
 const Gameboard = (size) => {
   // initialize empty board with empty cells to false
   const board = [...Array(size)].map(() => Array(size).fill(false));
@@ -59,11 +62,12 @@ const Gameboard = (size) => {
     return board
   }
 
-  const placeShip = (x, y, orient, ship) => {
-    ships.push(ship);
-    for(let i=0; i<ship.getLength(); i++) {
-      if(orient) board[x+i][y] = [ship, i];
-      else board[x][y+i] = [ship, i];
+  const placeShip = (x, y, orient, length) => {
+    let playerShip = (0,_Ship_js__WEBPACK_IMPORTED_MODULE_0__.ship)(length);
+    ships.push(playerShip);
+    for(let i=0; i<playerShip.getLength(); i++) {
+      if(orient) board[x+i][y] = [playerShip, i];
+      else board[x][y+i] = [playerShip, i];
     }
   }
 
@@ -99,6 +103,41 @@ const Gameboard = (size) => {
   }
 
   return { getBoard, placeShip, receiveAttack }
+}
+
+
+
+/***/ }),
+/* 3 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "ship": () => (/* binding */ ship)
+/* harmony export */ });
+const ship = (length) => {
+  let hits = new Array(length).fill(false);
+
+  const getLength = () => {
+    return length
+  }
+  
+  const hit = (square) => {
+    if(hits[square]) return false;
+    hits[square] = true;
+    return true;
+  }
+
+  const isSunk = () => {
+    let i = 0;
+    while(i < length) {
+      if(!hits[i]) return false;
+      i++;
+    }
+
+    return true;
+  }
+  return { getLength, hit, isSunk }
 }
 
 
@@ -167,27 +206,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Player_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 
 
-const render = () => {
-
-} 
-
-const renderBoard = (Gameboard) => {
-
-}
-
-const newGame = () => {
-  // create p1 player and computer player objects
-  let p1 = (0,_Player_js__WEBPACK_IMPORTED_MODULE_0__.player)();
-  let computer = (0,_Player_js__WEBPACK_IMPORTED_MODULE_0__.player)();
-
+const renderBoard = (player, playerObj, state) => {
   // get Gameboard to render to screen
-  const p1Board = document.getElementById('player')
-  const computerBoard = document.getElementById('computer')
+  const board = document.getElementById(player)
+  // clear board
+  board.innerHTML = ''
+  const playerBoard = playerObj.getBoard()
 
   let table = document.createElement('table')
   let tbody = document.createElement('tbody')
+  let title = document.createElement('h2')
 
-  let size = p1.getGameBoard().getBoard().length
+  let message = player
+  if(state === 'WIN' || state === 'LOSE') message = state
+  title.innerText = message
+
+  let size = playerBoard.length
 
   for(let i=0; i<size; i++) {
     let row = document.createElement('tr')
@@ -196,6 +230,30 @@ const newGame = () => {
       cell.dataset.y = i+1
       cell.dataset.x = String.fromCharCode(65+j)
       row.appendChild(cell)
+
+      if(playerBoard[i][j] === 'hit') {
+        cell.classList.add('sunk-cell')
+      }
+      else if(playerBoard[i][j] === 'miss') {
+        cell.classList.add('miss-cell')
+      }
+      else if(typeof playerBoard[i][j] === 'object' && playerBoard[i][j] != null) {
+        // currently using same name used for html id, maybe add cpu flag in gameboard/player functions to use?
+        if(player === 'computer' && state === 'playing') {
+          cell.classList.add('unchecked')
+          cell.addEventListener('click', function() { attack(i, j, playerObj, player) })
+        } 
+        else {
+          cell.classList.add('ship-cell')
+        }
+      }
+      else {
+        if(player === 'computer' && state === 'playing') {
+          cell.classList.add('unchecked')
+          cell.addEventListener('click', function() { attack(i, j, playerObj, player) })
+        }
+      }
+
       if(i===0) {
         let xAxisLabel = document.createElement('div')
         xAxisLabel.classList.add('x-axis-label')
@@ -213,20 +271,56 @@ const newGame = () => {
   }
 
   table.appendChild(tbody)
+  board.appendChild(title)
+  board.appendChild(table)
+}
 
-  p1Board.appendChild(table)
+const newGame = () => {
+  // create user player and computer player objects
+  let user = (0,_Player_js__WEBPACK_IMPORTED_MODULE_0__.player)()
+  let computer = (0,_Player_js__WEBPACK_IMPORTED_MODULE_0__.player)()
 
-  let clone = table.cloneNode(true)
-  computerBoard.appendChild(clone)
+  let userBoard = user.getGameBoard()
+  let computerBoard = computer.getGameBoard()
 
+  // manually place ships; to be later replaced with user input ship placement
+  userBoard.placeShip(0, 2, false, 3)
+  userBoard.placeShip(1, 6, false, 4)
+  userBoard.placeShip(2, 0, false, 2)
+  userBoard.placeShip(3, 7, true, 2)
+  userBoard.placeShip(4, 5, false, 1)
+  userBoard.placeShip(5, 0, false, 1)
+  userBoard.placeShip(6, 7, false, 3)
+  userBoard.placeShip(8, 1, false, 1)
+  userBoard.placeShip(8, 4, false, 1)
+  userBoard.placeShip(8, 8, true, 2)
 
-  p1.getGameBoard();
-  computer.getGameBoard();
+  computerBoard.placeShip(0, 2, false, 3)
+  computerBoard.placeShip(1, 6, false, 4)
+  computerBoard.placeShip(2, 0, false, 2)
+  computerBoard.placeShip(3, 7, true, 2)
+  computerBoard.placeShip(4, 5, false, 1)
+  computerBoard.placeShip(5, 0, false, 1)
+  computerBoard.placeShip(6, 7, false, 3)
+  computerBoard.placeShip(8, 1, false, 1)
+  computerBoard.placeShip(8, 4, false, 1)
+  computerBoard.placeShip(8, 8, true, 2)
+
+  renderBoard('user', userBoard, 'playing')
+  renderBoard('computer', computerBoard, 'playing')
   
   // Use event listener to get valid user input and run computer turn
   // using a timeout checking each time for a 'GAME OVER' where a 
   // game over screen will display and following a timeout clear and
   // display start screen
+}
+
+const attack = (x, y, playerObj, player) => {
+  let state = 'playing'
+  if(playerObj.receiveAttack(x, y) === 'GAME OVER') {
+    state = 'WIN'
+  }
+  renderBoard(player, playerObj, state)
 }
 
 const reset = () => {
